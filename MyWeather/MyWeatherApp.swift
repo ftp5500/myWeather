6239493b4c45a9b51c9baeae71d3cf2ebe58be2d
ftp5500 <ftp5500@yahoo.com>
2023-06-16 17:19:25 +0300

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Ali on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
